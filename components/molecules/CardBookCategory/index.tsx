d85d06ed61932663e7bed90e566345f265060c17
Rizanoor <rizanoorfauzan123@gmail.com>
2023-07-20 12:08:51 +0700

    fiix: slicing

---
+++ components/molecules/CardBookCategory/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <>
+      <div className="relative overflow-hidden bg-white shadow-md rounded-xl">
+        <a href="#">
+          <img
+            className="m-auto mt-8 mb-4 overflow-hidden w-36 h-44"
+            src="/images/negeriparabedebah.png"
+            alt="picturebook"
+          />
+        </a>
+        <div className="px-4 text-left">
+          <h5 className="overflow-hidden overflow-ellipsis whitespace-nowrap">
+            Negeri Para Bedebah
+          </h5>
+          <p className="">Tere Liye</p>
+          <p className="overflow-hidden overflow-ellipsis whitespace-nowrap p-small">
+            Kisah berawal dari Thomas sebagai seorang konsultan keuangan
+            yang sangat terkenal. Keahlian Thomas dalam bidang pekerjaannya
+            ini membuat dirinya bahkan sering diundang menjadi pembicara
+            dalam berbagai kesempatan seperti talk show dan seminar hingga
+            luar negeri. Thomas bisa dibilang merupakan salah satu figure
+            paling berpengaruh dalam bidang perekonomian hingga mampu
+            mendirikan firma konsultannya bernama Thomas & Co. Ketenaran
+            Thomas membuat banyak orang ingin mengulik sisi dari dirinya
+            yang amat misterius dan latar belakangnya yang gelap.
+          </p>
+          <span className="rating-more">
+            <img className="rating" src="/svg/star5.svg" alt="Rating" />
+            <a className="more" href="books/book1.html">
+              Selengkapnya
+            </a>
+          </span>
+        </div>
+      </div>
+    </>
+  )
+}
--- components/organisms/Header/title.tsx
+++ components/organisms/Header/title.tsx
@@ -4,18 +4,15 @@ export default function title() {
   return (
     <>
       <div className="mb-16 lg:mt-32 xl:mt-40 xl:mr-12">
-            <h1 className="mb-5 h1-large">Team management mobile application</h1>
-            <p className="mb-8 p-large">
-              Start getting things done together with your team based on Pavo's
-              revolutionary team management features
-            </p>
-            <a className="btn-solid-lg" href="#your-link"
-            ><i className="fab fa-apple"></i>Download</a
-            >
-            <a className="btn-solid-lg secondary" href="#your-link"
-            ><i className="fab fa-google-play"></i>Download</a
-            >
-          </div>
+        <h1 className="mb-5 h1-large">Team management mobile application</h1>
+        <p className="mb-8 p-large">
+          Start getting things done together with your team based on Pavo's
+          revolutionary team management features
+        </p>
+        <a className="btn-solid-lg" href="#your-link">
+          Cari buku
+        </a>
+      </div>
     </>
   )
 }
