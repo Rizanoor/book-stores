c93882828eeeb63545bc37572741ef42b1b53694
yogodwiansuryanto <yogodwiansuryanto@gmail.com>
2023-07-21 20:08:04 +0800

    menambah file API, Update cardbook & collection

--- components/molecules/CardBookCategory/index.tsx
+++ components/molecules/CardBookCategory/index.tsx
@@ -1,41 +1,52 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { fetchApi } from "@/components/services";
+import { useState, useEffect } from "react";
 
 export default function index() {
+  const [dataList, setDataList] = useState([]);
+  const [isLoading] = useState(false);
+
+  const getData = async () => {
+    const data = await fetchApi();
+    setDataList(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <>
-      <div className="relative overflow-hidden bg-white shadow-md rounded-xl">
-        <a href="/details">
-          <img
-            className="m-auto mt-8 mb-4 overflow-hidden w-36 h-44"
-            src="/images/negeriparabedebah.png"
-            alt="picturebook"
-          />
-        </a>
-        <div className="px-4 text-left">
-          <h5 className="overflow-hidden overflow-ellipsis whitespace-nowrap">
-            Negeri Para Bedebah
-          </h5>
-          <p className="">Tere Liye</p>
-          <p className="overflow-hidden overflow-ellipsis whitespace-nowrap p-small">
-            Kisah berawal dari Thomas sebagai seorang konsultan keuangan
-            yang sangat terkenal. Keahlian Thomas dalam bidang pekerjaannya
-            ini membuat dirinya bahkan sering diundang menjadi pembicara
-            dalam berbagai kesempatan seperti talk show dan seminar hingga
-            luar negeri. Thomas bisa dibilang merupakan salah satu figure
-            paling berpengaruh dalam bidang perekonomian hingga mampu
-            mendirikan firma konsultannya bernama Thomas & Co. Ketenaran
-            Thomas membuat banyak orang ingin mengulik sisi dari dirinya
-            yang amat misterius dan latar belakangnya yang gelap.
-          </p>
-          <span className="rating-more">
-            <img className="rating" src="/svg/star5.svg" alt="Rating" />
-            <Link className="more" href="/details">
-              Selengkapnya
-            </Link>
-          </span>
+      {isLoading ? <div>Loading</div> : ""}
+      {dataList.map((item) => (
+        <div
+          className="relative overflow-hidden bg-white shadow-md rounded-xl"
+          key={item.id}
+        >
+          <a href="/details">
+            <img
+              className="m-auto mt-8 mb-4 overflow-hidden w-36 h-44"
+              src={"images" + "/" + item.images}
+              alt="picturebook"
+            />
+          </a>
+          <div className="px-4 text-left">
+            <h5 className="overflow-hidden overflow-ellipsis whitespace-nowrap">
+              {item.title}
+            </h5>
+            <p className="">{item.penulis}</p>
+            <p className="overflow-hidden overflow-ellipsis whitespace-nowrap p-small">
+              {item.deskripsi}
+            </p>
+            <span className="rating-more">
+              <img className="rating" src="/svg/star5.svg" alt="Rating" />
+              <Link className="more" href="/details">
+                Selengkapnya
+              </Link>
+            </span>
+          </div>
         </div>
-      </div>
+      ))}
     </>
-  )
+  );
 }
--- pages/allbook.tsx
+++ pages/allbook.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import CardBookCategory from '@/components/molecules/CardBookCategory'
+import Navbar from '@/components/organisms/Navbar'
 
 export default function allbook() {
   return (
     <>
+    <Navbar/>
+    <div className='pt-20'>
       <div className="pt-8 pb-12 bg-gray">
         <div className="container px-4 sm:px-8">
           <div className="flex justify-between">
@@ -15,16 +18,10 @@ export default function allbook() {
 
           <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
             <CardBookCategory />
-            <CardBookCategory />
-            <CardBookCategory />
-            <CardBookCategory />
-            <CardBookCategory />
-            <CardBookCategory />
-            <CardBookCategory />
-            <CardBookCategory />
           </div>
         </div>
       </div>
+    </div>
     </>
   )
 }
