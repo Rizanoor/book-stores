9cca5fdefd2ff74ace251fc0d8c76a5dc9fa8ff4
galangramadan <galangynr@gmail.com>
2023-07-22 13:02:31 +0700

    feat: card book props

--- components/molecules/CardBookCategory/index.tsx
+++ components/molecules/CardBookCategory/index.tsx
@@ -1,52 +1,38 @@
 import Link from "next/link";
 import React from "react";
-import { fetchApi } from "@/components/services";
-import { useState, useEffect } from "react";
 
-export default function index() {
-  const [dataList, setDataList] = useState([] as bookDetails[]);
-  const [isLoading] = useState(false);
+export default function index(props: bookDetails) {
+  const { id, title, penulis, deskripsi, images } = props;
 
-  const getData = async () => {
-    const data = await fetchApi();
-    setDataList(data);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
   return (
     <>
-      {isLoading ? <div>Loading</div> : ""}
-      {dataList.map((item) => (
-        <div
-          className="relative overflow-hidden bg-white shadow-md rounded-xl"
-          key={item.id}
-        >
-          <Link href={`/book/${item.id}`}>
-            <img
-              className="m-auto mt-8 mb-4 overflow-hidden w-36 h-44"
-              src={"images" + "/" + item.images}
-              alt="picturebook"
-            />
-          </Link>
-          <div className="px-4 text-left">
-            <h5 className="overflow-hidden overflow-ellipsis whitespace-nowrap">
-              {item.title}
-            </h5>
-            <p className="">{item.penulis}</p>
-            <p className="overflow-hidden overflow-ellipsis whitespace-nowrap p-small">
-              {item.deskripsi}
-            </p>
-            <span className="rating-more">
-              <img className="rating" src="/svg/star5.svg" alt="Rating" />
-              <Link className="more" href={`/book/${item.id}`}>
-                Selengkapnya
-              </Link>
-            </span>
-          </div>
+      <div
+        className="relative overflow-hidden bg-white shadow-md rounded-xl"
+        key={props.id}
+      >
+        <Link href={`/book/${id}`}>
+          <img
+            className="m-auto mt-8 mb-4 overflow-hidden w-36 h-44"
+            src={"images" + "/" + images}
+            alt="picturebook"
+          />
+        </Link>
+        <div className="px-4 text-left">
+          <h5 className="overflow-hidden overflow-ellipsis whitespace-nowrap">
+            {title}
+          </h5>
+          <p className="">{penulis}</p>
+          <p className="overflow-hidden overflow-ellipsis whitespace-nowrap p-small">
+            {deskripsi}
+          </p>
+          <span className="rating-more">
+            <img className="rating" src="/svg/star5.svg" alt="Rating" />
+            <Link className="more" href={`/book/${id}`}>
+              Selengkapnya
+            </Link>
+          </span>
         </div>
-      ))}
+      </div>
     </>
   );
 }
--- components/organisms/Collection/index.tsx
+++ components/organisms/Collection/index.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import CardBookCategory from '@/components/molecules/CardBookCategory'
-import Link from 'next/link';
+import React, { useEffect, useState } from "react";
+import CardBookCategory from "@/components/molecules/CardBookCategory";
+import Link from "next/link";
+import { fetchApi } from "@/components/services";
 
 export default function index() {
+  const [dataList, setDataList] = useState([] as bookDetails[]);
+
+  const getData = async () => {
+    const data = await fetchApi();
+    setDataList(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="pt-8 pb-12 bg-gray">
       <div className="container px-4 sm:px-8">
@@ -14,7 +26,19 @@ export default function index() {
         </div>
 
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-          <CardBookCategory />
+          {Object.keys(dataList).length > 0 &&
+            dataList.map((item, i) => (
+              <div key={i}>
+                <CardBookCategory
+                  id={item.id}
+                  title={item.title}
+                  penulis={item.penulis}
+                  deskripsi={item.deskripsi}
+                  images={item.images}
+                  kategori={item.kategori}
+                />
+              </div>
+            ))}
         </div>
       </div>
     </div>
